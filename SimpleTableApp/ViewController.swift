22e0710245937b5a5600bfb3ec4d4c56325bd2a2
Boris <boris@macos.shared>
2022-06-20 12:09:39 -0500

    Initial Commit

---
+++ SimpleTableApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleTableApp
+//
+//  Created by Boris on 6/20/22.
+//  Copyright © 2022 Boris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
